2df979d2871a139d0af9323163b6839052ded32d
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-18 17:42:42 +0300

    configure database settings to use postgres

--- hoodapp/settings.py
+++ hoodapp/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'hoodapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'nasseh',
+        'PASSWORD': 'nasseh82473',
     }
 }
 
@@ -108,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
