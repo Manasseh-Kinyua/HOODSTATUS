54193225c9955d251cb1109ad5c0e5245d82c49d
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-18 20:38:27 +0300

    Add business and post classes and migrate the database

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,5 @@
+import email
+from email import message
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
@@ -14,3 +16,22 @@ class Neighborhood(models.Model):
     
     def __str__(self):
         return self.name
+
+class Business(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    hood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    message = models.TextField()
+    image = models.ImageField(upload_to='photos/', null=True)
+    create = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    hood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message
