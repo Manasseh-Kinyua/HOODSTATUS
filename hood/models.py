9ad8f9496c8ac0e41673a6ad5e5e80e70304239a
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-18 18:04:58 +0300

    create a neighborhood class

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    description = models.TextField(null=True)
+    occupants = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
