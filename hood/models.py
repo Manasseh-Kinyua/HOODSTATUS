b677ee546c04bd9f98a6518263279a8871684060
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-19 08:43:44 +0300

    add the profile class and migrate the database

--- hood/models.py
+++ hood/models.py
@@ -6,6 +6,13 @@ import uuid
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='photos/', null=True)
+    bio = models.TextField(null=True)
+    hood = models.ForeignKey('Neighborhood', on_delete=models.CASCADE, null=True)
+
+
 class Neighborhood(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
