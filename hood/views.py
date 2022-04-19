dcb750439bcaff90a1d9477b6d703086fd280813
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-04-19 08:03:14 +0300

    add the login view function with all the login mfunctionality

--- hood/views.py
+++ hood/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render, redirect
 from .models import Neighborhood
 from .forms import HoodForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def loginpage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+
+        else:
+            messages.error(request, 'Username or Password does not exist')
     context = {}
     return render(request, 'hood/login_register.html', context)
 
